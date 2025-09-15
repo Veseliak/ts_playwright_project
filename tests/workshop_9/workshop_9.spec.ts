40479d67e5bc0599253ac36b5077008807e9a3fc
iveseliak <ivan.veseliak@xperi.com>
2025-09-15 23:06:10 +0200

    update tests, removed only

--- tests/workshop_9/workshop_9.spec.ts
+++ tests/workshop_9/workshop_9.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('Automating Form Submission @githubAction', async ({ page }) => {
+test('Automating Form Submission @githubAction', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/');
 
   const newTodo = await page.getByPlaceholder('What needs to be done?');
@@ -19,7 +19,7 @@ test.only('Automating Form Submission @githubAction', async ({ page }) => {
   await page.waitForTimeout(3000);
 });
 
-test.only('Handling Form @githubAction', async ({ page }) => {
+test('Handling Form @githubAction', async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc/');
 
   await page.fill('[placeholder = "What needs to be done?"]', 'John Due');
